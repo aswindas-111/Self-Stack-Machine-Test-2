0938f9af8ee1fa65c2bcd665a0b96e50924191d4
ASWINDAS <cvaswin1997@gmail.com>
2024-05-30 23:38:28 +0530

    1st qs added

---
+++ 1-stack using a list.py
@@ -0,0 +1,15 @@
+stack = []
+
+stack.append(5)
+stack.append(10)
+stack.append(15)
+
+print("current stack = ", stack)
+
+print("\n elements getting removed from stack -> ")
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print("\n Stack after elements are poped")
+print(stack)
